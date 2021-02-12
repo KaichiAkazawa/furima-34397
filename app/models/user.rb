7f29135d0fcec810156bc9fe4459705333ad7331
KaichiAkazawa <kic.jump@gmail.com>
2021-02-12 19:08:00 +0900

    新規登録画面作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
   #全角カタカナでの入力必須
   NAME_RUBY_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
-  validates :encrypted_password, format: { with: PASSWORD_REGEX, message: "Password Include both letters and numbers" }
+  validates :encrypted_password, format: { with: PASSWORD_REGEX, message: "Include both letters and numbers" }
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: NAME_REGEX, message: "Last name Full-width characters" }
-  validates :first_name, presence: true, format: { with: NAME_REGEX, message: "First name Full-width characters" }
-  validates :last_name_ruby, presence: true, format: { with: NAME_RUBY_REGEX, message: "Last name Full-width katakana characters" }
-  validates :first_name_ruby, presence: true, format: { with: NAME_RUBY_REGEX, message: "First name Full-width katakana characters" }
+  validates :last_name, presence: true, format: { with: NAME_REGEX, message: "Full-width characters" }
+  validates :first_name, presence: true, format: { with: NAME_REGEX, message: "Full-width characters" }
+  validates :last_name_ruby, presence: true, format: { with: NAME_RUBY_REGEX, message: "Full-width katakana characters" }
+  validates :first_name_ruby, presence: true, format: { with: NAME_RUBY_REGEX, message: "Full-width katakana characters" }
   validates :birth_day, presence: true
 end
