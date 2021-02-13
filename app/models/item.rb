bd023ffb595b118bc7e48f0a09d3d620228a5703
KaichiAkazawa <kic.jump@gmail.com>
2021-02-13 18:49:36 +0900

    Itemモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :state_id
+    validates :shipping_cost_payer_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price, 
+                numericality: { only_integer: true, 
+                  greater_than_or_equal_to: 300, 
+                  less_than_or_equal_to: 9999999 }
+  end
 end
