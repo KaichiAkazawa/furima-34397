b8fe3bcd9aff544fc3570b456373071f04a2d8e8
KaichiAkazawa <kic.jump@gmail.com>
2021-02-15 13:48:31 +0900

    販売手数料/利益の表示修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,11 +2,11 @@ function price () {
   const itemPrice = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
     const addTaxPrice = document.getElementById("add-tax-price");
+    const addTaxPriceValue = Math.floor(itemPrice.value * 0.1);
     const profit = document.getElementById("profit");
-    const addTaxPriceValue = itemPrice.value * 0.1;
-    const priceValue = itemPrice.value - addTaxPriceValue;
+    const profitValue = itemPrice.value - addTaxPriceValue;
     addTaxPrice.innerHTML = addTaxPriceValue;
-    profit.innerHTML = priceValue;
+    profit.innerHTML = profitValue;
   });
 };
 
