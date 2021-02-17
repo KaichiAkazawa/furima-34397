f1e2adb2152dff92b37173d1c6169968c1caafc1
KaichiAkazawa <kic.jump@gmail.com>
2021-02-17 18:08:45 +0900

    Formオブジェクト作成

---
+++ app/models/purchase_order.rb
@@ -0,0 +1,24 @@
+class PurchaseOrder
+
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number, :purchase_id :user_id, :item_id
+
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
+  PHONE_NUMBER_REGEX = /\A\d{,11}\z/.freeze
+  with_options presence: true do
+    validates :postal_code, format: { with: POSTAL_CODE_REGEX, message: "Input correctly" }
+    validates :prefecture_id
+    validates :city
+    validates :address_number
+    validates :phone_number, format: { with: PHONE_NUMBER_REGEX, message: "Input only number" }
+    validates :purchase_id
+    validates :user_id
+    validates :item_id
+  end
+
+    def save
+      purchase = Purchase.create(user_id: user_id, item_id: item_id)
+      Order.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,
+        address_number: address_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)
+    end
+end
