453899d481a688344f8e3f4fed1dcd99e5051676
KaichiAkazawa <kic.jump@gmail.com>
2021-02-17 16:03:46 +0900

    Orderモデル作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,13 @@
 class Order < ApplicationRecord
-  
+  belongs_to :purchase
+
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
+  PHONE_NUMBER_REGEX = /\A\d{,11}\z/.freeze
+  with_options presence: true do
+    validates :postal_code, format: { with: POSTAL_CODE_REGEX, message: "" }
+    validates :prefecture_id
+    validates :city
+    validates :address_number
+    validates :phone_number, format: { with: PHONE_NUMBER_REGEX, message: "" }
+  end
 end
