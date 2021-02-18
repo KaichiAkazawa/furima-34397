bd6278f7c50f19c4795be78718cb426fdb60ef55
KaichiAkazawa <kic.jump@gmail.com>
2021-02-18 16:55:11 +0900

    クレジット決済サーバー側実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,33 @@
 class OrdersController < ApplicationController
   def index
+    @purchase_order = PurchaseOrder.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @purchase_order = PurchaseOrder.new(purchase_params)
+    if @purchase_order.valid?
+      pay_item
+      @purchase_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number
+    ).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
   end
 end
