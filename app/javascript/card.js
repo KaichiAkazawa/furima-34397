7ec4945edddeba43113fff9d3aad5a006c529168
KaichiAkazawa <kic.jump@gmail.com>
2021-02-18 16:57:56 +0900

    クレジット決済クライアントサイド修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,10 +8,10 @@ const pay = () => {
     const formData = new FormData(formResult);
 
     const card = {
-      number: formData.get("order[number]") ,
-      exp_month: formData.get("order[exp_month]"),
-      exp_year: `20${formData.get("order[exp_year]")}`,
-      cvc: formData.get("order[cvc]"),
+      number: formData.get("purchase_order[number]") ,
+      exp_month: formData.get("purchase_order[exp_month]"),
+      exp_year: `20${formData.get("purchase_order[exp_year]")}`,
+      cvc: formData.get("purchase_order[cvc]"),
     };
 
     Payjp.createToken(card, (status, response) => {
@@ -19,10 +19,10 @@ const pay = () => {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type='hidden'>`;
-        renderDom.insertAdjacentElement("beforeend", tokenObj);
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       };
 
-      document.getElementById("card_number").removeAttribute("name")
+      document.getElementById("card-number").removeAttribute("name")
       document.getElementById("card-exp-month").removeAttribute("name")
       document.getElementById("card-exp-year").removeAttribute("name")
       document.getElementById("card-cvc").removeAttribute("name")
