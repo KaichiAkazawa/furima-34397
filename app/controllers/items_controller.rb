2f28a378d36194e02e9cd82b9c8235cd3e7118a4
KaichiAkazawa <kic.jump@gmail.com>
2021-02-19 01:23:01 +0900

    購入/未購入での表示内容変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -53,6 +53,6 @@ class ItemsController < ApplicationController
   end
 
   def redirect_root
-    redirect_to root_path unless @item.user == current_user
+    redirect_to root_path unless @item.user == current_user && @item.purchase.nil?
   end
 end
