5ad0d933b42739ea3acdc7be2ee0df04d055f521
KaichiAkazawa <kic.jump@gmail.com>
2021-02-19 01:23:38 +0900

    rubocop修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :redirect_root
+
   def index
     @purchase_order = PurchaseOrder.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_order = PurchaseOrder.new(purchase_params)
     if @purchase_order.valid?
       pay_item
@@ -17,17 +19,27 @@ class OrdersController < ApplicationController
   end
 
   private
+
   def purchase_params
-    params.require(:purchase_order).permit(:postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number
+    params.require(:purchase_order).permit(
+      :postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number
     ).merge(token: params[:token], user_id: current_user.id, item_id: @item.id)
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_root
+    redirect_to root_path if current_user == @item.user || !@item.purchase.nil?
   end
 end
--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,16 +1,16 @@
 class PurchaseOrder
-
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number, :token, :purchase_id, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :address_number, :building_name, :phone_number, :token, :purchase_id,
+                :user_id, :item_id
 
-  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/.freeze
+  POSTAL_CODE_REGEX = /\A\d{3}-\d{4}\z/.freeze
   PHONE_NUMBER_REGEX = /\A\d{,11}\z/.freeze
   with_options presence: true do
-    validates :postal_code, format: { with: POSTAL_CODE_REGEX, message: "Input correctly" }
+    validates :postal_code, format: { with: POSTAL_CODE_REGEX, message: 'Input correctly' }
     validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :city
     validates :address_number
-    validates :phone_number, format: { with: PHONE_NUMBER_REGEX, message: "Input only number" }
+    validates :phone_number, format: { with: PHONE_NUMBER_REGEX, message: 'Input only number' }
     validates :token
     validates :user_id
     validates :item_id
@@ -19,6 +19,7 @@ class PurchaseOrder
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Order.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city,
-      address_number: address_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+                 address_number: address_number, building_name: building_name,
+                 phone_number: phone_number, purchase_id: purchase.id)
   end
 end
