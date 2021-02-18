dda433bb80fc4cf5b4eacda697e4b1c8baf8346c
KaichiAkazawa <kic.jump@gmail.com>
2021-02-19 01:24:06 +0900

    purchase_ordeer単体テストコード作成

---
+++ spec/factories/purchase_orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :purchase_order do
+    postal_code { '123-4567' }
+    prefecture_id { rand(2..48) }
+    city { '横浜市緑区' }
+    address_number { '青山1-1-1' }
+    phone_number { '09012345678' }
+    token { 'tok_abcdefghijk00000000000000000' }
+  end
+end
